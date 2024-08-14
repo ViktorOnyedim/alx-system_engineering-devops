1076ca99999f75f5ef086c3d18cca9036a6598c3
ViktorOnyedim <viktoronyedim@gmail.com>
2024-08-14 04:52:33 +0000

    Added 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    endpoint = f"https://www.reddit.com/r/{subreddit}/about.json"
+    
+    headers = {'User-Agent': "my_alx:com.api_advanced:v1 (by /u/Downtown-Baby6380)"}
+    response = requests.get(endpoint, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
+
+
