8a0799fd5918c697bf0065fc8773ec58270f7d1f
ViktorOnyedim <viktoronyedim@gmail.com>
2024-08-14 06:29:42 +0000

    Modified 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,26 +4,21 @@
 """
 import requests
 
+
 def top_ten(subreddit):
-    """ Prints the titles of the first 10 hot posts listed for a given subreddit."""
+    """Print the titles of the first 10 hottest posts listed for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'my_alx:com.api_advanced:v1.0.0 (by /u/Do    wntown-Baby6380)'}
-    params = {'limit': 10}
+    headers = {
+        'User-Agent': 'my_alx:com.api_advanced:v1.0.0 (by /u/Downtown-Baby6380)',
+    }
+    params = {
+        "limit": 10,
+    }
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-    if response.status_code == 200:
-        # Parse json data
-        data = response.json()
-        # get children object that contains the posts
-        posts = data["data"]["children"]
-        
-        if posts:
-            for i, post in enumerate(posts):
-                if i < 10:
-                    print(post['data']['title'])
-                else:
-                    break
-    else:
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
         return
+    data = response.json().get("data")
+    [print(post.get("data").get("title")) for post in data.get("children")]
