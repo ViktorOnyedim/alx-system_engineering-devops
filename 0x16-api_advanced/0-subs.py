23268a24fa1098fed13680b2e59b66985b2c252c
ViktorOnyedim <viktoronyedim@gmail.com>
2024-08-14 04:59:37 +0000

    Updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,10 @@ queries the Reddit API and returns the number of subscribers (not active users, 
 import requests
 
 def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a givven subreddit."""
     endpoint = f"https://www.reddit.com/r/{subreddit}/about.json"
     
-    headers = {'User-Agent': "my_alx:com.api_advanced:v1 (by /u/Downtown-Baby6380)"}
+    headers = {'User-Agent': "my_alx:com.api_advanced:v1.0.0 (by /u/Downtown-Baby6380)"}
     response = requests.get(endpoint, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
