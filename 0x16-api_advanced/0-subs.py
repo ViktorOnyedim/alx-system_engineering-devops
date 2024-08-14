05e78e2ec5489718b49dcd45ae25d648ec36f14f
ViktorOnyedim <viktoronyedim@gmail.com>
2024-08-14 05:04:44 +0000

    Modified 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,13 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a givven subreddit."""
     endpoint = f"https://www.reddit.com/r/{subreddit}/about.json"
     
-    headers = {'User-Agent': "my_alx:com.api_advanced:v1.0.0 (by /u/Downtown-Baby6380)"}
+    headers = {'User-Agent': 'my_alx:com.api_advanced:v1.0.0 (by /u/Downtown-Baby6380)'}
     response = requests.get(endpoint, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+        #data = response.json()
+        data = response.json().get('data').get('subscribers')
+        return data
     else:
         return 0
 
