0e9b46fea25141b96c668bccca0cea47dc12f655
ViktorOnyedim <viktoronyedim@gmail.com>
2024-09-19 15:02:18 +0000

    Modified 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,43 +1,29 @@
 #!/usr/bin/python3
-
+"""
+Using reddit's API recursively
+"""
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
-    # set a custom user-agent to avoid potential api issues
-    headers = {'User-Agent': 'MyAgent/01'}
+    """Returning top post titles recursively"""
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, params=parameters, headers=user_agent, allow_redirects=False)
 
-    # base url for reddit api
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    if response.status_code == 200:
+        data = response.json().get("data")
+        after = data.get("after")
+        all_titles = data.get("children")
+        
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
 
-    # Add 'after' parameter if it's provided
-    params = {'limit': 100}
-    if after:
-        params['after'] = after
-
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-        # Check if the subreddit is invalid
-        if response.status_code in [404, 302]:
-            return None
-
-        # Raise exception for any other unsuccessful status code
-        response.raise_for_status()
-
-        data = response.json()
-
-        posts = data['data']['children']
-        hot_list.extend([post['data']['title'] for post in posts])
-
-        # check if there are more pages
-        after = data['data']['after']
-        if after:
-            # recursively call the function with the new 'after' value
+        # Continue recursion if there is another page of posts
+        if after is not None:
             return recurse(subreddit, hot_list, after)
         else:
             return hot_list
-    except request.RequestException:
+    else:
         return None
-
-
-
