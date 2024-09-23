e4bc71aa81f5f9871afca3f69e3d9c87ff638a36
ViktorOnyedim <viktoronyedim@gmail.com>
2024-09-23 06:49:14 +0000

    Removed try/except block

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
+queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit. 
+If an invalid subreddit is given, the function should return 0.
 """
 
 import requests
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a givven subreddit."""
+    """Return the total no. of subscribers on a givven subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     
     headers = {
         'User-Agent': 'api_advanced/v1.0 (by /u/Downtown-Baby6380)'
     }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+    res = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
 
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+    if res.status_code == 200:
+        data = res.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
+        
